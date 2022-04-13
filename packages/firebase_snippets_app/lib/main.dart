6e76135d2270ff0640f971082cfd0945d34b0d13
Eric Windmill <ewindmill@google.com>
2022-04-13 12:07:27 -0400

    adds emulator inits

--- packages/firebase_snippets_app/lib/main.dart
+++ packages/firebase_snippets_app/lib/main.dart
@@ -16,11 +16,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 // [END set_up_environment]
 
 import 'package:firebase_snippets_app/app.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -59,7 +61,12 @@ void main() async {
     // [END auth_persistingAuthState]
   }
 
-  if (!kReleaseMode) db.useFirestoreEmulator('localhost', 8080);
+  if (!kReleaseMode) {
+    FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    FirebaseDatabase.instance.useDatabaseEmulator('localhost', 9000);
+    FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
+  }
 
   runApp(
     MyApp(
