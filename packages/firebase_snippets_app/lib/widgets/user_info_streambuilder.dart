299329fb74672af768f343f08bd9809183c299e8
Kevin Cheung <kevincheung@google.com>
2022-05-23 10:18:05 -0700

    Add Firestore+StreamBuilder example

---
+++ packages/firebase_snippets_app/lib/widgets/user_info_streambuilder.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+// [START listen_to_realtime_updates_listen_for_updates2]
+class UserInformation extends StatefulWidget {
+  @override
+  _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['full_name']),
+                  subtitle: Text(data['company']),
+                );
+              })
+              .toList()
+              .cast(),
+        );
+      },
+    );
+  }
+}
+// [EMD listen_to_realtime_updates_listen_for_updates2]
