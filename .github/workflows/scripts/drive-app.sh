619f0d0302a4f21726faf2c3a6bc080bce7b0daf
Cynthia J <cynthiajoan@gmail.com>
2024-10-08 11:22:31 -0700

    try again with win32 pinned to higher version

--- .github/workflows/scripts/drive-app.sh
+++ .github/workflows/scripts/drive-app.sh
@@ -5,9 +5,7 @@ TARGET_PATH="packages/$PROJECT_NAME/"
 
 # The && are necessary for some reason when using the Android Emulator action
 pushd "$TARGET_PATH" || exit &&
-flutter pub upgrade win32 &&
 flutter clean &&
-flutter channel stable &&
 flutter pub get &&
 dart format . &&
 flutter test integration_test
