116a7c430ded71f45838ef2f71bebcffdb639b73
Cynthia J <cynthiajoan@users.noreply.github.com>
2024-10-08 10:23:01 -0700

    Update drive-app.sh
    Try to fix the ci issue https://github.com/firebase/snippets-flutter/actions/runs/11157277326/job/31011251101?pr=36

--- .github/workflows/scripts/drive-app.sh
+++ .github/workflows/scripts/drive-app.sh
@@ -5,7 +5,8 @@ TARGET_PATH="packages/$PROJECT_NAME/"
 
 # The && are necessary for some reason when using the Android Emulator action
 pushd "$TARGET_PATH" || exit &&
-flutter clean &&
+flutter pub cache clean &&
+flutter channel stable &&
 flutter pub get &&
 dart format . &&
 flutter test integration_test
